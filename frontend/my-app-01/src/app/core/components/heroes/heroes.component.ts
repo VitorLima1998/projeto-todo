d5e76dc38c97f802244c2d3dce90212239733b8b
VitorLima1998 <vitor.xlr8@gmail.com>
2022-11-20 14:32:36 -0400

    project in progress

--- frontend/my-app-01/src/app/core/components/heroes/heroes.component.ts
+++ frontend/my-app-01/src/app/core/components/heroes/heroes.component.ts
@@ -4,7 +4,8 @@ import { MatTable } from '@angular/material/table';
 import { HeroInsertDialogComponent } from '../hero-insert-dialog/hero-insert-dialog.component';
 import { Hero } from '../../model/hero';
 import { HeroService } from '../../services/hero.service';
-import { MatTabsModule } from '@angular/material/tabs';
+import { PanelMenuModule } from 'primeng/panelmenu';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-heroes',
@@ -12,71 +13,74 @@ import { MatTabsModule } from '@angular/material/tabs';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  displayedColumns: string[] = ['id', 'name'];
+  // displayedColumns: string[] = ['name'];
 
   selectedHero?: Hero;
   id?: string;
   name?: string;
   dataSource: Hero[] = [];
 
-  @ViewChild(MatTable) table!: MatTable<Hero>;
-
-  displayColumns: string[] = ['id', 'name', 'action'];
-
-  constructor(private heroService: HeroService, public dialog: MatDialog) {}
-
-  openDialog(): void {
-    const dialogRef = this.dialog.open(HeroInsertDialogComponent, {
-      width: '250px',
-      data: { id: this.id, name: this.name },
-    });
-
-    dialogRef.afterClosed().subscribe((result) => {
-      if (result !== undefined) {
-        // let id = this.heroService.maxId() + 1;
-        let hero: Hero = { name: result };
-
-        this.heroService.addHero(hero).subscribe({
-          next: (hero) => {
-            console.log(hero);
-            this.getHeroes(); // Atualizar o data Source
-          },
-          error: (err) => {
-            console.error(err);
-          },
-        });
-        // this.heroService.addHeroes(hero).subscribe((heroes) => {
-        //   console.log(heroes);
-        //   // this.dataSource = heroes;
-        // });
-
-        this.table.renderRows();
-      }
-    });
-  }
-
-  removeData() {
-    this.dataSource.pop();
-    this.table.renderRows();
-  }
-
-  ngOnInit(): void {
-    this.getHeroes();
-  }
-
-  selectHero(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
-  getHeroes(): void {
-    // this.dataSource = this.heroService.getHeroes();
-    // this.HEROES = this.heroService.getHeroes();
-    this.heroService.getHeroes().subscribe({
-      next: (data) => {
-        this.dataSource = data;
-        // console.log(data);
+  ngOnInit() {
+    this.dataSource = [
+      {
+        name: 'File',
+        // icon: 'pi pi-pw pi-file',
+        power: 'teste',
       },
-      error: (e) => console.error(e),
-    });
+    ];
   }
+  //   @ViewChild(MatTable) table!: MatTable<Hero>;
+
+  //   displayColumns: string[] = ['name'];
+
+  //   constructor(private heroService: HeroService, public dialog: MatDialog) {}
+
+  //   openDialog(): void {
+  //     const dialogRef = this.dialog.open(HeroInsertDialogComponent, {
+  //       width: '250px',
+  //       data: { id: this.id, name: this.name },
+  //     });
+
+  //     dialogRef.afterClosed().subscribe((result) => {
+  //       if (result !== undefined) {
+  //         // let id = this.heroService.maxId() + 1;
+  //         let hero: Hero = { name: result };
+
+  //         this.heroService.addHero(hero).subscribe({
+  //           next: (hero) => {
+  //             console.log(hero);
+  //             this.getHeroes(); // Atualizar o data Source
+  //           },
+  //           error: (err) => {
+  //             console.error(err);
+  //           },
+  //         });
+
+  //         this.table.renderRows();
+  //       }
+  //     });
+  //   }
+
+  //   removeData() {
+  //     this.dataSource.pop();
+  //     this.table.renderRows();
+  //   }
+
+  //   ngOnInit(): void {
+  //     this.getHeroes();
+  //   }
+
+  //   selectHero(hero: Hero): void {
+  //     this.selectedHero = hero;
+  //   }
+
+  //   getHeroes(): void {
+  //     this.heroService.getHeroes().subscribe({
+  //       next: (data) => {
+  //         this.dataSource = data;
+  //         // console.log(data);
+  //       },
+  //       error: (e) => console.error(e),
+  //     });
+  //   }
 }
--- frontend/my-app-01/src/app/primeng/primeng.module.ts
+++ frontend/my-app-01/src/app/primeng/primeng.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ToastModule } from 'primeng/toast';
 import { AccordionModule } from 'primeng/accordion';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ButtonModule } from 'primeng/button';
 import { PanelMenuModule } from 'primeng/panelmenu';
+import { TabViewModule } from 'primeng/tabview';
 
 const COMPONENTS = [
   ToastModule,
@@ -14,6 +16,8 @@ const COMPONENTS = [
   ToastModule,
   ButtonModule,
   PanelMenuModule,
+  TabViewModule,
+  CommonModule,
 ];
 
 @NgModule({
